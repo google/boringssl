5833bcd8acd3ae5aba7e9462e94a325146bfe08a
xfding <xfding@google.com>
2026-02-12 13:47:40 +0000

    rust: move rustls adapters to its own crate
    Signed-off-by: Xiangfei Ding <xfding@google.com>
    Change-Id: I943ef1f202223b18a8331a830b564d2e6a6a6964
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/89171
    Reviewed-by: Rudolf Polzer <rpolzer@google.com>

--- rust/bssl-rustls-adapters/src/lib.rs
+++ rust/bssl-rustls-adapters/src/lib.rs
@@ -21,11 +21,14 @@
     clippy::expect_used
 )]
 #![allow(private_bounds)]
-#![cfg_attr(not(any(feature = "std", test)), no_std)]
 
-//! BoringSSL TLS bindings
+//! BoringSSL-backed [`rustls`] adapters.
 //!
-//! *WARNING* this crate is still work in progress.
+//! This crate provides a [`rustls::crypto::CryptoProvider`] backed by
+//! BoringSSL, for use with the [`rustls`] TLS stack. See
+//! [`rustls_provider`] for details and examples.
 
 extern crate alloc;
 extern crate core;
+
+pub mod rustls_provider;
