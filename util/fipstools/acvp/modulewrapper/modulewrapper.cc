google/boringssl

commit 305bcfce00b189f2297f53365b0454f96009927d

util/fipstools: saltLen arg for ACVP RSA/sigGen/pss cmd
The ACVP capabilities for RSA sigGen allows specifying a PSS sigType
saltLen between 0 and the digest size (in bytes) of the associated
hashAlg.

This commit updates acvptool to provide the saltLen value as an argument
to the RSA/sigGen/$DIGEST/pss command for the module wrapper to process.

Change-Id: I552e65ad6ebed610a8545651af3c63fa3b46fef9
Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/88467
Reviewed-by: Xiangfei Ding <xfding@google.com>
Commit-Queue: Xiangfei Ding <xfding@google.com>
Reviewed-by: Rudolf Polzer <rpolzer@google.com>
Reviewed-by: Adam Langley <agl@google.com>

util/fipstools/acvp/modulewrapper/modulewrapper.cc
@@ -1860,6 +1860,17 @@ static bool RSASigGen(const Span<const uint8_t> args[],
   std::vector<uint8_t> sig(RSA_size(key));
   size_t sig_len;
   if (UsePSS) {
+    uint32_t salt_len;
+    if (args[2].size() != sizeof(salt_len)) {
+      return false;
+    }
+    memcpy(&salt_len, args[2].data(), sizeof(salt_len));
+    if (salt_len != digest_len) {
+      LOG_ERROR(
+          "PSS salt length %u does not match digest length %u.\n",
+          static_cast<unsigned>(salt_len), static_cast<unsigned>(digest_len));
+      return false;
+    }
     if (!RSA_sign_pss_mgf1(key, &sig_len, sig.data(), sig.size(), digest_buf,
                            digest_len, md, md, RSA_PSS_SALTLEN_DIGEST)) {
       return false;

util/fipstools/acvp/acvptool/subprocess/rsa.go
@@ -71,6 +71,7 @@ type rsaSigGenGroup struct {
 	SigType     string          `json:"sigType"`
 	ModulusBits uint32          `json:"modulo"`
 	Hash        string          `json:"hashAlg"`
+	SaltLen     uint32          `json:"saltLen"`
 	Tests       []rsaSigGenTest `json:"tests"`
 }
 
@@ -206,6 +207,10 @@ func processSigGen(vectorSet []byte, m Transactable) (any, error) {
 			return nil, fmt.Errorf("RSA SigGen test group has type %q, but only generation tests (%q) are supported", group.Type, expectedType)
 		}
 
+		if group.SigType != "pss" && group.SaltLen > 0 {
+			return nil, fmt.Errorf("RSA SigGen test group %d has sig type %s but saltLen %d - not 0", group.ID, group.SigType, group.SaltLen)
+		}
+
 		response := rsaSigGenTestGroupResponse{
 			ID: group.ID,
 		}
@@ -220,7 +225,12 @@ func processSigGen(vectorSet []byte, m Transactable) (any, error) {
 				return nil, fmt.Errorf("test case %d/%d contains invalid hex: %s", group.ID, test.ID, err)
 			}
 
-			m.TransactAsync(operation, 3, [][]byte{uint32le(group.ModulusBits), msg}, func(result [][]byte) error {
+			args := [][]byte{uint32le(group.ModulusBits), msg}
+			if group.SigType == "pss" {
+				args = append(args, uint32le(group.SaltLen))
+			}
+
+			m.TransactAsync(operation, 3, args, func(result [][]byte) error {
 				if len(response.N) == 0 {
 					response.N = hex.EncodeToString(result[0])
 					response.E = hex.EncodeToString(result[1])

commit 520e1a284598bd1d95c933749939c84dd751d2c1

fips-20260721: Fix ACVP RSA PSS.
305bcfce0 broke ACVP PSS because it didn't update the expected number of
arguments for the operation in modulewrapper.cc. We didn't notice
because we didn't have ACVP tests for RSA signature generation. Fix both
things.

(cherry picked from commit ef0c0723156ff8014feb6b6789afe1e43d7e8710)
Change-Id: I224a6626e9a9ecdf4c21838aa55425bae1b72139
Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/100947
Auto-Submit: Adam Langley <agl@google.com>
Commit-Queue: Xiangfei Ding <xfding@google.com>
Reviewed-by: Xiangfei Ding <xfding@google.com>
SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>

util/fipstools/acvp/modulewrapper/modulewrapper.cc
@@ -2535,12 +2535,12 @@ static constexpr struct {
     {"RSA/sigGen/SHA2-384/pkcs1v1.5", 2, RSASigGen<EVP_sha384, false>},
     {"RSA/sigGen/SHA2-512/pkcs1v1.5", 2, RSASigGen<EVP_sha512, false>},
     {"RSA/sigGen/SHA-1/pkcs1v1.5", 2, RSASigGen<EVP_sha1, false>},
-    {"RSA/sigGen/SHA2-224/pss", 2, RSASigGen<EVP_sha224, true>},
-    {"RSA/sigGen/SHA2-256/pss", 2, RSASigGen<EVP_sha256, true>},
-    {"RSA/sigGen/SHA2-384/pss", 2, RSASigGen<EVP_sha384, true>},
-    {"RSA/sigGen/SHA2-512/pss", 2, RSASigGen<EVP_sha512, true>},
-    {"RSA/sigGen/SHA2-512/256/pss", 2, RSASigGen<EVP_sha512_256, true>},
-    {"RSA/sigGen/SHA-1/pss", 2, RSASigGen<EVP_sha1, true>},
+    {"RSA/sigGen/SHA2-224/pss", 3, RSASigGen<EVP_sha224, true>},
+    {"RSA/sigGen/SHA2-256/pss", 3, RSASigGen<EVP_sha256, true>},
+    {"RSA/sigGen/SHA2-384/pss", 3, RSASigGen<EVP_sha384, true>},
+    {"RSA/sigGen/SHA2-512/pss", 3, RSASigGen<EVP_sha512, true>},
+    {"RSA/sigGen/SHA2-512/256/pss", 3, RSASigGen<EVP_sha512_256, true>},
+    {"RSA/sigGen/SHA-1/pss", 3, RSASigGen<EVP_sha1, true>},
     {"RSA/sigVer/SHA2-224/pkcs1v1.5", 4, RSASigVer<EVP_sha224, false>},
     {"RSA/sigVer/SHA2-256/pkcs1v1.5", 4, RSASigVer<EVP_sha256, false>},
     {"RSA/sigVer/SHA2-384/pkcs1v1.5", 4, RSASigVer<EVP_sha384, false>},